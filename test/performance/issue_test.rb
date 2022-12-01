ee120b010d27d9c517800592d416ae873d5b0eeb
Alexander Meindl <alex@alphanodes.com>
2022-12-01 12:01:22 +0100

    some unsuccessfully work on performance tests

--- test/performance/issue_test.rb
+++ test/performance/issue_test.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-require 'rails/performance_test_help'
 
-class IssuesTest < ActionDispatch::PerformanceTest
+class IssueTest < ActionDispatch::PerformanceTest
   # Refer to the documentation for all available options
   # self.profile_options = { runs: 5, metrics: [:wall_time, :memory],
   #                          output: 'tmp/performance', formats: [:flat] }
 
-  test 'time_entries' do
-    get '/time_entries'
+  test 'issues' do
+    @request.session[:user_id] = 2
+    get '/issues'
+  end
+
+  test 'issue' do
+    @request.session[:user_id] = 2
+    get '/issues/1'
   end
 end
